291440892a7425d512ee8ac9ee14ff0a5d814a88
xiaqb <xiaqiubo123@163.com>
2024-05-06 10:26:25 +0800

    feat: 增加飞书机器人通知

---
+++ bots/flybook.js
@@ -0,0 +1,39 @@
+const axios = require('axios');
+
+/**
+ * 
+ * @param {Array<String>} urls 
+ * @param {object} params 
+ * @param {String} params.name
+ * @param {Number} params.id
+ * @param {String} params.time
+ * @param {String} params.msg
+ * @param {String} params.times 单位时间内触发次数
+ * @param {String} params.delay 延迟触发间隔
+ * @returns 
+ */
+module.exports = function send(urls, params) {
+    if (global.debug) console.log('飞书 待发送', urls)
+    const title = `进程: ${params.name} ${params.id}(${params.delay}秒发生${params.times}次)`
+    for (const url of urls) {
+        if (!url) continue
+        const u = new URL(url);
+        axios.post(url, {
+            msg_type: 'interactive',
+            card: {
+                elements: [
+                    {
+                        tag: "markdown",
+                        content: `<font color='grey'>${params.msg}</font>`,
+                    },
+                ],
+                header: {
+                    title: {
+                        content: title,
+                        tag: "plain_text"
+                    }
+                }
+            }
+        }).catch(console.error)
+    }
+}
